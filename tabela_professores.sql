9db6dc69b40517d4c1958a809ef62bcf631bcc22
Guilherme Ribeiro (GR) <its.gr6603@gmail.com>
2023-04-06 14:40:30 -0300

    new deploy

--- tabela_professores.sql
+++ tabela_professores.sql
@@ -19,3 +19,19 @@ INSERT INTO id_materia (descricao) VALUES ("Ciência da Computação");
 INSERT INTO id_materia (descricao) VALUES ("Banco de Dados");
 
 SELECT * from materia;
+
+INSERT INTO professor (nome, email, id_materia) VALUES ("Ferdinanda", "ferdinanda@escola.pr.gov.br", 1);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Helena", "helena@escola.pr.gov.br", 2);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Marcia", "marcia@escola.pr.gov.br", 3);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Domingos", "domingos@escola.pr.gov.br", 4);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Deolindo", "deolindo@escola.pr.gov.br", 5);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Fernanda", "fernanda@escola.pr.gov.br", 6);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Idjarrury", "idjarrury@escola.pr.gov.br", 7);
+INSERT INTO professor (nome, email, id_materia) VALUES ("João", "joao@escola.pr.gov.br", 8);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Viviani", "viviani@escola.pr.gov.br", 9);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Cleverson", "cleverson@escola.pr.gov.br", 10);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Cleide", "cleide@escola.pr.gov.br", 11);
+INSERT INTO professor (nome, email, id_materia) VALUES ("William", "william@escola.pr.gov.br", 12);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Edvaldo", "edvaldo@escola.pr.gov.br", 13);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Osvaldo", "osvaldo@escola.pr.gov.br", 14);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Edvaldo", "edvaldo@escola.pr.gov.br", 15);
